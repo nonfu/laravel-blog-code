c24fd69144904f0bd2a757e6e78276b2bbfdd070
qiang.sun <qiang.sun@husor.com>
2019-01-08 12:58:57 +0800

    文章列表API接口

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    public function index(Request $request)
+    {
+        // 每页显示5条记录
+        $posts = Post::orderBy('published_at', 'desc')->simplePaginate(5);
+        $items = [];
+        foreach ($posts->items() as $post) {
+            $item['id'] = $post->id;
+            $item['title'] = $post->title;
+            $item['summary'] = $post->subtitle;
+            $item['thumb'] = config('blog.uploads.webpath') . '/' . $post->page_image;
+            $item['posted_at'] = $post->published_at;
+            $item['views'] = mt_rand(1, 10000); // 暂时没有实现文章浏览数逻辑，返回随机数
+            $items[] = $item;
+        }
+        $data = [
+            'message' => 'success',
+            'articles' => $items
+        ];
+        return response()->json($data);
+    }
+}
